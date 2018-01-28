eb78eaddc812323ff3cd7e7fb14e41408aedfde2
Glitch (remix-corner) <none>
2018-01-28 00:28:47 +0000

    ☄️🍂 Checkpoint README.md:242011/23 public/glitch-corner.js:242011/74 server.js:242011/15 package.json:242011/1

---
+++ public/glitch-corner.js
@@ -0,0 +1,7 @@
+() => {
+  var projectName = location.hostname.split('.')[0];
+
+document.write( '<a href="https://glitch.com/edit/#!/remix/' + projectName + '">' +
+                '<img width="40" height="26" src="https://glitch.com/edit/images/logos/glitch/logo-day.svg" style="position: absolute; top: 20; border: 0; right: 10; transform: rotate(45deg); z-index: 100;">' +
+                '</a>');
+}
