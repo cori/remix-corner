e726708f164005e925f3146943ef721f4afec5bf
Glitch (remix-corner) <none>
2018-01-28 05:15:02 +0000

    🏞🍇 Checkpoint public/glitch-corner.js:242011/75 README.md:242011/112

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # glitchCorner
 
-Example of Glitch logo in the corner
+Example of Glitch logo in the corner, linking directly to a remix of the current project. A mashup inspired by 
 
 Repo available [here](https://github.com/deprecatedcoder/glitchCorner)
--- public/glitch-corner.js
+++ public/glitch-corner.js
@@ -1,7 +1,7 @@
 (function() {
   var projectName = location.hostname.split('.')[0];
 
-document.write( '<a href="https://glitch.com/edit/#!/remix/' + projectName + '">' +
-                '<img width="40" height="26" src="https://glitch.com/edit/images/logos/glitch/logo-day.svg" style="position: absolute; top: 20; border: 0; right: 10; transform: rotate(45deg); z-index: 100;">' +
+document.write( '<a href="https://glitch.com/edit/#!/remix/' + projectName + '" title="Remix me on Glitch!">' +
+                '<img width="40" height="26" alt="Remix on Glitch" src="https://glitch.com/edit/images/logos/glitch/logo-day.svg" style="position: absolute; top: 20; border: 0; right: 10; transform: rotate(45deg); z-index: 100;">' +
                 '</a>');
 })();
